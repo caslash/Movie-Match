6cd8fc307444a19f71f59ba0bdd41c4bfd01e48a
Cameron <caslash@bsu.edu>
2022-04-08 20:52:54 -0400

    Initial Commit

---
+++ MovieMatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieMatch
+//
+//  Created by Cameron Slash on 08/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieMatch/MovieMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMatchApp.swift
+//  MovieMatch
+//
+//  Created by Cameron Slash on 08/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
